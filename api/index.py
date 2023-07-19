707b5b32e82a1bcea2807ccd6b53f55a591013c2
TechnoCoder08 <jansky.stepan08@gmail.com>
2023-07-19 21:57:40 +0200

    i fucking hate my life

--- api/index.py
+++ api/index.py
@@ -72,6 +72,7 @@ def handle_incoming():
             data_raw = data_in.replace("{", "").replace("}", "").replace("'", "").replace(":", "_")
             #print(data_raw)
             data = data_raw.split("_")
+            return data
             # if data[0] == 'fill':
             #     print("filling")
             #     for y in range(100):
